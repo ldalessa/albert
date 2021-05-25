07700e2f09fad669f2e835a7b05a1fa5974e2843
Luke D'Alessandro <ldalessa@uw.edu>
2021-05-25 11:13:55 -0700

    Use customization-point style calls to find cmath functions.

--- include/albert/cmath.hpp
+++ include/albert/cmath.hpp
@@ -65,28 +65,48 @@ namespace albert
       return dim_v<A>;
     }
 
-    constexpr auto evaluate(ScalarIndex<rank_v<CMath>>) const
+    constexpr auto evaluate(ScalarIndex<rank_v<CMath>> const&) const
     {
+      using std::abs;
+      using std::exp;
+      using std::log;
+      using std::sqrt;
+      using std::sin;
+      using std::cos;
+      using std::tan;
+      using std::asin;
+      using std::acos;
+      using std::atan;
+      using std::atan2;
+      using std::sinh;
+      using std::cosh;
+      using std::tanh;
+      using std::asinh;
+      using std::acosh;
+      using std::atanh;
+      using std::ceil;
+      using std::floor;
+
       switch (tag) {
-       case ABS:   return std::abs(a.evaluate());
-       case EXP:   return std::exp(a.evaluate());
-       case LOG:   return std::log(a.evaluate());
-       case SQRT:  return std::sqrt(a.evaluate());
-       case SIN:   return std::sin(a.evaluate());
-       case COS:   return std::cos(a.evaluate());
-       case TAN:   return std::tan(a.evaluate());
-       case ASIN:  return std::asin(a.evaluate());
-       case ACOS:  return std::acos(a.evaluate());
-       case ATAN:  return std::atan(a.evaluate());
-       case ATAN2: return std::atan2(a.evaluate());
-       case SINH:  return std::sinh(a.evaluate());
-       case COSH:  return std::cosh(a.evaluate());
-       case TANH:  return std::tanh(a.evaluate());
-       case ASINH: return std::asinh(a.evaluate());
-       case ACOSH: return std::acosh(a.evaluate());
-       case ATANH: return std::atanh(a.evaluate());
-       case CEIL:  return std::ceil(a.evaluate());
-       case FLOOR: return std::floor(a.evaluate());
+       case ABS:   return abs(a.evaluate());
+       case EXP:   return exp(a.evaluate());
+       case LOG:   return log(a.evaluate());
+       case SQRT:  return sqrt(a.evaluate());
+       case SIN:   return sin(a.evaluate());
+       case COS:   return cos(a.evaluate());
+       case TAN:   return tan(a.evaluate());
+       case ASIN:  return asin(a.evaluate());
+       case ACOS:  return acos(a.evaluate());
+       case ATAN:  return atan(a.evaluate());
+       case ATAN2: return atan2(a.evaluate());
+       case SINH:  return sinh(a.evaluate());
+       case COSH:  return cosh(a.evaluate());
+       case TANH:  return tanh(a.evaluate());
+       case ASINH: return asinh(a.evaluate());
+       case ACOSH: return acosh(a.evaluate());
+       case ATANH: return atanh(a.evaluate());
+       case CEIL:  return ceil(a.evaluate());
+       case FLOOR: return floor(a.evaluate());
        default:
         __builtin_abort();
       }
@@ -121,12 +141,15 @@ namespace albert
       return dim_v<A>;
     }
 
-    constexpr auto evaluate(ScalarIndex<rank_v<CMath2>>) const
+    constexpr auto evaluate(ScalarIndex<rank_v<CMath2>> const&) const
     {
+      using std::fmin;
+      using std::fmax;
+      using std::pow;
       switch (tag) {
-       case FMIN: return std::fmin(a.evaluate(), b.evaluate());
-       case FMAX: return std::fmax(a.evaluate(), b.evaluate());
-       case POW:  return std::pow(a.evaluate(), b.evaluate());
+       case FMIN: return fmin(a.evaluate(), b.evaluate());
+       case FMAX: return fmax(a.evaluate(), b.evaluate());
+       case POW:  return pow(a.evaluate(), b.evaluate());
        default:
         __builtin_abort();
       }
